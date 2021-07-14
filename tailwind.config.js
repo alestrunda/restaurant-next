826a946a997b9ea3db11ee7e105353d30596491d
Aleš Trunda <alestrunda@gmail.com>
2021-07-14 21:31:27 +0200

    disable purge for development

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  mode: "all",
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
