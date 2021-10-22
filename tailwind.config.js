14365cbceb2f12b8878fbab7dc3e64cee1c679bb
Aleš Trunda <alestrunda@gmail.com>
2021-10-22 23:06:10 +0200

    add containers/ path to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   mode: "all",
   purge: {
-    enabled: process.env.NODE_ENV === 'production',
+    enabled: process.env.NODE_ENV === "production",
     content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
+      "./containers/**/*.{js,ts,jsx,tsx}",
     ],
   },
   darkMode: false, // or 'media' or 'class'
