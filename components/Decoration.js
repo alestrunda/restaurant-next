0fa2fa65ff5a29420972b3db8195f207f1a76fe6
Aleš Trunda <alestrunda@gmail.com>
2021-10-22 22:58:54 +0200

    use fixed classnames

--- components/Decoration.js
+++ components/Decoration.js
@@ -1,11 +1,18 @@
 import classNames from "classnames";
 
+const sizeMap = {
+  5: "h-5",
+  6: "h-6",
+  7: "h-7",
+  8: "h-8",
+};
+
 const Decoration = ({ className, color = "gold", size = 6 }) => (
   <div
     className={classNames(
       "bg-no-repeat bg-center",
       color && `decoration-${color}`,
-      `h-${size}`,
+      sizeMap[size],
       className
     )}
   ></div>
