2e1959087b2fb4abc8eff828085fb378ef6f1b65
Aleš Trunda <alestrunda@gmail.com>
2021-10-22 23:10:22 +0200

    add sections/ path to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === "production",
     content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
       "./containers/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./sections/**/*.{js,ts,jsx,tsx}",
     ],
   },
   darkMode: false, // or 'media' or 'class'
