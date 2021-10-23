2e43b10d34170e736fbd6d5b6d1bec7fa5c39d5a
Aleš Trunda <alestrunda@gmail.com>
2021-10-23 21:12:16 +0200

    add menu page content

--- pages/menu.js
+++ pages/menu.js
@@ -1,11 +1,12 @@
 import Header from "../components/Header";
 import Page from "../components/Page";
 import Footer from "../sections/Footer";
+import MenuSection from "../sections/Menu";
 
 const Menu = () => (
   <Page>
     <Header />
-    TODO
+    <MenuSection />
     <Footer />
   </Page>
 );
