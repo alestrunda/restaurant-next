3cd0075b61b1d96312ed63162b31fe0987a316b9
Aleš Trunda <alestrunda@gmail.com>
2021-12-31 17:43:17 +0100

    fix logo path

--- pages/layout/Header.js
+++ pages/layout/Header.js
@@ -7,7 +7,7 @@ const Header = ({ children, isOverlay }) => {
     <>
       <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-b from-black to-transparent opacity-80"></div>
       <div className="container flex justify-between mx-auto relative z-20 px-14">
-        <img className="self-center" src="img/logo.png" />
+        <img className="self-center" src="/img/logo.png" />
         <Navigation />
       </div>
     </>
